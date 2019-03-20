695cab186e542ea3bb0c2831497908ca2c2e4890
Shankar Manoharan <shankar.manoharan@singtel.com>
2019-03-20 17:51:41 +0800

    first commit

---
+++ git-localconfig-repo/spring-cloud-config-service/src/test/java/com/g2s/rest/microservices/springcloudconfigservice/SpringCloudConfigServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.g2s.rest.microservices.springcloudconfigservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConfigServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
